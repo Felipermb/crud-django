e59eee3689b13d34eb9006bb39ea4295036612a6
felipermb <felipe_rdmd@hotmail.com>
2016-11-01 17:19:35 -0300

    Em andamento

--- virtual/lavanderia/autenticacao/views.py
+++ virtual/lavanderia/autenticacao/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class Autenticacao(View):
     """
@@ -10,14 +13,26 @@ class Autenticacao(View):
         return render(request, 'autenticacao/login.html', {})
 
     def post(self, request):
-        	print request.POST.get("login")
-        	login = request.POST.get("login")
-        	print request.POST.get("senha")
-        	senha = request.POST.get("senha")
-		return render(request, 'autenticacao/login.html', {})
+        resposta = {
+            'sucesso': False,
+            'mensagem': ''
+        }
+        usuario = request.POST.get("login")
+        senha = request.POST.get("senha")
+        user = authenticate(username=usuario, password=senha)
+        if user:
+            login(request, user)
+            return redirect('/index')
+        else:
+            resposta['mensagem'] = 'Login ou Senha incorreto(s)'
 
-class Index(View):
+        return render(request, 'autenticacao/login.html', resposta)
+
+class Index(LoginRequiredMixin, View):
+    login_url = '/'
+
     def get(self, request):
+        
         return render(request, 'index.html', {})
 
     def post(self, request):
